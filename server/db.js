7e89fcead1878fc99688d63c60d7cc0e05d1707d
dh3b <ozootly@gmail.com>
2026-02-11 17:17:17 +0100

    Modify server constants

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import pg from "pg";
 const pool = new pg.Pool({
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME || "proservices",
+  database: process.env.DB_NAME || "briefservice",
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
 });
